9d8301d15d474e490585cc31de8057eadc9b3464
GiovannySissa <helber.sissa@mail.escuelaing.edu.co>
2024-08-26 07:51:54 -0500

    CH5 complete section 5.15 to 5.19

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -1,10 +1,11 @@
 module Ch5 where
 
-import Prelude (Unit, (+), show, discard)
+import Prelude (Unit, (+), (==), show, discard)
 
 import Data.List (List(..), (:))
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Maybe (Maybe(..))
 
 
 flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
@@ -76,6 +77,28 @@ length'' l = go 0 l where
   go acc (_: xs) = go (1 + acc) xs
 
 
+{-
+  Section 5.16 to 5.19
+-}
+
+head :: ∀ a. List a -> Maybe a 
+head Nil = Nothing
+head (x : _) = Just x
+
+tail :: ∀ a. List a -> Maybe (List a)
+tail Nil = Nothing
+tail (_ : xs) = Just (xs)
+
+tail' :: ∀ a. List a -> List a
+tail' Nil = Nil
+tail' (_ : xs) = xs
+
+last :: ∀ a. List a -> Maybe a
+last Nil = Nothing
+last (x) = 
+  if length (x) == 1 then     
+    head x 
+  else last (tail' x)
 
 
 test :: Effect Unit
@@ -86,6 +109,13 @@ test = do
   -- flip const 1 2 # show # log
   -- log $ show $null Nil
   -- log $ show $null ("xyz" : Nil)
-  log $ show $ snoc (1 : 2 : Nil) 3
-  log $ show $ length $ 1 : 2 : 3 : Nil
-  log $ show $ length'' $ 1 : 2 : 3 : Nil
+  -- log $ show $ snoc (1 : 2 : Nil) 3
+  -- log $ show $ length $ 1 : 2 : 3 : Nil
+  -- log $ show $ length'' $ 1 : 2 : 3 : Nil
+  -- log $ show $ head (Nil :: List Unit)
+  -- log $ show $ head ("abc" : "123" : Nil) 
+  -- log $ show $ (tail Nil :: Maybe (List Unit))
+  -- log $ show $ tail ("abc" : "123" : "xyz" : Nil) 
+  log $ show $ (last Nil :: Maybe Unit)
+  log $ show $ last ("a" : "b" : "c" : Nil)
+  log $ show $ last ("z" : Nil)
