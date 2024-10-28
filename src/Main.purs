089f0a4e65c0addd50ae76336363faf3ba63f3ad
GiovannySissa <helber.sissa@mail.escuelaing.edu.co>
2024-10-28 12:22:55 -0500

    CH 17 validation

--- src/Main.purs
+++ src/Main.purs
@@ -13,7 +13,8 @@ import Effect (Effect)
 -- import Ch14 as Ch14
 -- import Ch15 as Ch15
 -- import Ch16 as Ch16
-import Ch17 as Ch17
+-- import Ch17 as Ch17
+import Parser as P
 
 main :: Effect Unit
-main = Ch17.test
+main = P.test
